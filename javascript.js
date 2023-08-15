c0ed4de1a041793220b366c712df2b3704984f1a
LuluBui <laskanumberthree@gmail.com>
2023-08-15 18:59:06 -0400

    complete about.html, portfolio.html, added images, did more css

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
 console.log("Hello World");
+const navButtons = document.getElementsByTagName('li');
+// if(li.matches(':hover')){
+//     console.log("hmm");
+// }
+for(let i = 0; i < navButtons.length; i++) {
+    if(navButtons[i].matches(':hover')) {
+        console.log("hmm");
+    }
+}
